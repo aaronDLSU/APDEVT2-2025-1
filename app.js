fdbd0122dbac596f26effc1a0df658d63c5310f4
aaron <ronny_co@dlsu.edu.ph>
2025-02-20 18:09:03 +0800

    fixed app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const expressLayout = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//Change to public if changing the folder location
+//Change to 'public' if changing the folder location
 app.use(express.static('root'));
 
 // Template
@@ -16,3 +16,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'))
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
--- server/routes/main.js
+++ server/routes/main.js
@@ -6,8 +6,4 @@ router.get('/', (req, res) => {
     res.send('Hello World!');
 });
   
-router.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-});
-
 module.exports = router;
