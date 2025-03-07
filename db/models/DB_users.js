f307a1ed53b32f34c88e841f048861b1d3643459
S16AbeaA <adrian_bea@dlsu.edu.ph>
2025-03-07 23:37:40 +0800

    fixed user db

--- db/models/DB_users.js
+++ db/models/DB_users.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, default: 'student', unique: true },
     name: { type: String},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },                 // Should be hashed
