3d4a4ccea393d8f4970aa03928f9ea7ec2526b74
S16ABeaA <159003129+S16ABeaA@users.noreply.github.com>
2025-03-23 13:42:22 +0800

    Update DB_users.js

--- db/models/DB_users.js
+++ db/models/DB_users.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    userId: {
-        type: Number,
-        unique: true
-      },
     username: {type: String},
     name: { type: String},
     email: { type: String, required: true, unique: true },
