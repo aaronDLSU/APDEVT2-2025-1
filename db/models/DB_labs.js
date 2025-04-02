bd4de2c1cd280c2aa444d231e9e2f78999058642
S16ABeaA <159003129+S16ABeaA@users.noreply.github.com>
2025-04-02 23:49:41 +0800

    Update DB_labs.js

--- db/models/DB_labs.js
+++ db/models/DB_labs.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const LabSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    building: { type: String, required: true, unique: true },
+    building: { type: String, required: true },
     capacity: { type: Number, required: true }, 
     availability: { type: Boolean, default: true }, 
     createdAt: { type: Date, default: Date.now }
