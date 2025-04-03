13bc5743e8f990524c7466cfa7a69beacf240475
mika <mikaelaamon@gmail.com>
2025-04-03 14:42:57 +0800

    update default

--- db/models/DB_settings.js
+++ db/models/DB_settings.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const SettingSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     accVisibility: { type: String, enum: ['Public', 'Private'] , default: 'Public' },
-    showStats: { type: Boolean, default: false },
-    showReserves: { type: Boolean, default: false },
-    showBooked: { type: Boolean, default: false },
-    showOngoing: { type: Boolean, default: false },
-    showPrevious: { type: Boolean, default: false },
+    showStats: { type: Boolean, default: true },
+    showReserves: { type: Boolean, default: true },
+    showBooked: { type: Boolean, default: true },
+    showOngoing: { type: Boolean, default: true },
+    showPrevious: { type: Boolean, default: true },
 });
 
 const Settings = mongoose.model('Setting', SettingSchema);
