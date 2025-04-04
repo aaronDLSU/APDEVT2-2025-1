5be3c6a6e5ab2d0872048940eee4679ad0b2001d
S16ABeaA <159003129+S16ABeaA@users.noreply.github.com>
2025-04-04 12:57:12 +0800

    Update main.js

--- server/routes/main.js
+++ server/routes/main.js
@@ -13,18 +13,19 @@ router.use(cookieParser());
 
 router.use(
     session({
-           secret: "secret-key",
+      secret: "secret-key",
       resave:false,
       saveUninitialized: false,
-
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+      }),
       proxy: true,
       cookie:{
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24
       }
-    })  
-    })
+    });  
   );
 
 const isAuthenticated = (req, res, next) => {
