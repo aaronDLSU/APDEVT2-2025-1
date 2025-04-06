4766956066c649066221a22982352279e54996de
migsalvador <miguelmanansalasalvador@gmail.com>
2025-04-06 23:54:21 +0800

    Removed profilepicid field

--- db/models/DB_users.js
+++ db/models/DB_users.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String},
-    name: { type: String},
+    username: { type: String },
+    name: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },                 // Should be hashed
-    profilePic: { type: String, default: '/images/default_profilepic.jpg'}, //for profile picture
+    profilePic: { type: String, default: '/images/default_profilepic.jpg' }, //for profile picture
     description: { type: String },
     role: { type: String, enum: ['student', 'labtech'], default: 'student' },
     isActivated: { type: Boolean, default: true },             // If account for deletion: can deactivate first
     activationToken: { type: String },                          // Token for emails activation / verification; can delete
     createdAt: { type: Date, default: Date.now },
-    profilePicId: { type: mongoose.Schema.Types.ObjectId } //GridFS profile pic
 });
 
 const User = mongoose.model('User', UserSchema);
